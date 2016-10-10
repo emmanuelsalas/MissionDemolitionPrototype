6c56d881098692a08ea489f7512fc661a192bae2
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-09 21:04:51 -0400

    Save1
    P.457 - Showing When Slingshot is Active

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	void OnMouseEnter(){
+		print ("Slingshot:OnMouseEnter()");
+	}
+	void OnMouseExit(){
+		print ("Slingshot:OnMouseExit()");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
