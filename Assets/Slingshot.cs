6bac0e78b971c71de61859826e7d79ef91fec788
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-12 21:28:49 -0400

    Problem 1
    Cannot select prefab in Slingshot

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -1,22 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Slingshot : MonoBehaviour {
+public class Slingshot : MonoBehaviour{
+	//fields set in the Unity Inspector pane
+	public GameObject prefabProjectile;
+	public bool _________;
+	//fields set dynamically
+	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+
+	void Awake(){
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+	}
 
 	void OnMouseEnter(){
-		print ("Slingshot:OnMouseEnter()");
-	}
-	void OnMouseExit(){
-		print ("Slingshot:OnMouseExit()");
+		//print ("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive(true);
 	}
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnMouseExit(){
+		//print ("Slingshot:OnMouseExit()");
+		launchPoint.SetActive(false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		
+	void OnMousedown(){
+		//The player has pressed the mouse button while over Slingshot
+		aimingMode = true;
+		//Instantiate a projectile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+		//Start it at the launchPoint
+		projectile.transform.position = launchPos;
+		//Set it to isKinematic for now
+		projectile.rigidbody.isKinematic = true;
 }
